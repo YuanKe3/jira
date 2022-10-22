bbdada7bf97653ee636b62435ffbe95340acf25b
Yuanxinyue <cctvyxy@qq.coom>
2022-10-22 14:27:44 +0800

    style: 安装 emotion,优化登录注册界面

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './App.css'
 import { loadDevTools } from 'jira-dev-tool'
+// 务必在 jira-dev-tool 中引入
+import 'antd/dist/antd.less'
 import { AppProviders } from 'context'
 
 loadDevTools(() =>
