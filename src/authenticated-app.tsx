05fbbe4700559094d20f6a957af4a05ab52b3321
Yuanxinyue <cctvyxy@qq.coom>
2022-10-22 14:56:47 +0800

    feat: 用 Row 布局页面 6.6

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { Row } from 'components/lib'
 import { useAuth } from 'context/auth-context'
 import { ProjectListScreen } from 'screens/project-list'
 
@@ -6,22 +7,19 @@ export const AuthenticatedApp = () => {
   const { logout } = useAuth()
   return (
     <Container>
-      <Header>
-        <HeaderLeft>
-          <h3>Logo</h3>
-          <h3>项目</h3>
-          <h3>用户</h3>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
         </HeaderLeft>
         <HeaderRight>
           <button onClick={logout}>登出</button>
         </HeaderRight>
       </Header>
-      <Nav>nav</Nav>
       <Main>
         <ProjectListScreen />
       </Main>
-      <Aside>aside</Aside>
-      <Footer>footer</Footer>
     </Container>
   )
 }
@@ -29,38 +27,13 @@ export const AuthenticatedApp = () => {
 const Container = styled.header`
   display: grid;
   grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas: 'header header header' 'nav main aside' 'footer footer footer';
   height: 100vh;
-  gap: 10rem;
 `
 
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`
+const Header = styled(Row)``
 
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`
+const HeaderLeft = styled(Row)``
 
 const HeaderRight = styled.div``
 
-const Main = styled.main`
-  grid-area: main;
-`
-
-const Nav = styled.nav`
-  grid-area: nav;
-`
-
-const Aside = styled.aside`
-  grid-area: aside;
-`
-
-const Footer = styled.footer`
-  grid-area: footer;
-`
+const Main = styled.main``
