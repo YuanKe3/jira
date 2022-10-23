162ac5dd1599e088b593530f182615f7a5d4438d
Yuanxinyue <cctvyxy@qq.coom>
2022-10-23 11:56:16 +0800

    feat: useMemo 的使用 8.6

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,20 +3,24 @@ import { Typography } from 'antd'
 import { useState } from 'react'
 import { useDebounce, useDocumentTitle } from 'utils'
 import { useProjects } from 'utils/project'
+import { useUrlQueryParam } from 'utils/url'
 import { useUsers } from 'utils/user'
 import { List } from './list'
 import { SearchPanel } from './search-panel'
 
 export const ProjectListScreen = () => {
   // 项目名称以及其负责人 id
-  const [param, setParam] = useState({
+  const [, setParam] = useState({
     name: '',
     personId: ''
   })
+  const [param] = useUrlQueryParam(['name', 'personId'])
   const debouncedParam = useDebounce(param, 200)
   const { isLoading, error, data: list } = useProjects(debouncedParam)
   const { data: users } = useUsers()
   useDocumentTitle('项目列表', false)
+
+  console.log(useUrlQueryParam(['name', 'id']))
 
   return (
     <Container>
@@ -27,6 +31,8 @@ export const ProjectListScreen = () => {
     </Container>
   )
 }
+
+ProjectListScreen.whyDidYouRender = true
 
 const Container = styled.div`
   padding: 3.2rem;
---
+++ src/utils/url.ts
@@ -0,0 +1,24 @@
+import { useMemo } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+/**
+ * 返回页面 url 中，指定键的参数值
+ */
+export const useUrlQueryParam = <K extends string>(keys: K[]) => {
+  const [searchParams, setSearchParam] = useSearchParams()
+  return [
+    useMemo(
+      () =>
+        keys.reduce(
+          (prev, key) => ({
+            ...prev,
+            [key]: searchParams.get(key) || ''
+          }),
+          {} as { [key in K]: string }
+        ),
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [searchParams]
+    ),
+    setSearchParam
+  ] as const
+}
